a0cdd79599119d532a4ab9856ba17407e92c605a
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-01-21 11:52:39 -0500

    commit #6 JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,7 +1,7 @@
 //  1) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import App from './App'
+import MyApp from './App'
 
 //  2) Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -10,4 +10,6 @@ const root = ReactDom.createRoot(el);
 //  4) Create a component
 
 //  5) show the component on the screen
-root.render(<App />)
+root.render(<MyApp />)
+
+// https://jsx-notes.vercel.app/#Returning%20JSX
