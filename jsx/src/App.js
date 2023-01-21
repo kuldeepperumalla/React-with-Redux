aca580551fe3f9ea5c33d18f130b0258a49f8402
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-01-21 11:30:38 -0500

    commit #5

---
+++ jsx/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+    return <h1>From App component</h1>
+}
+
+export default App
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,15 +1,13 @@
 //  1) Import the React and ReactDOM libraries
 import React from 'react';
-import ReactDom from 'react-dom/client'
+import ReactDom from 'react-dom/client';
+import App from './App'
 
 //  2) Get a reference to the div with ID root
 const el = document.getElementById('root');
 //  3) Tell React to take controll of that element
 const root = ReactDom.createRoot(el);
 //  4) Create a component
-function App(){
-  
- return <textarea autoFocus={true}/>
-}
+
 //  5) show the component on the screen
 root.render(<App />)
